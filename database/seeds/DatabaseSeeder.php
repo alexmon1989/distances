664c90338afa0ef04192cf94a014a6b2a64a06c3
alexmon1989 <alex.mon1989@gmail.com>
2016-05-04 10:44:53 +0300

    feature: Добавлены seeds для статьи на главной странице.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,6 +28,16 @@ class DatabaseSeeder extends Seeder
             'full_text' => 'About text',
         ]);
 
+        Memory::put('site.welcome_article_ru', [
+            'title' => 'Рассчитать расстояние между городами',
+            'full_text' => '<p>Расчет расстояний между городами всех стран мира. Основной приоритет в нашей работе — актуальность информации и точность расчета расстояний. Наша карта автомобильных дорог помогает быстро определять расстояния между городами. Благодарим за интерес к нашему сервису, всегда рады быть полезными для Вас!</p>',
+        ]);
+
+        Memory::put('site.welcome_article_en', [
+            'title' => 'Calculate the distance between cities',
+            'full_text' => '<p>Calculation of distances between cities all over the world. The main priority of our work - the relevance of information and distance calculation accuracy. Our map of roads helps to quickly determine distances between cities. Thank you for your interest in our service, always happy to be of assistance to you!</p>',
+        ]);
+
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
