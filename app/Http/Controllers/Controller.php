e1aa6446b75c418755ec91fbd373b14f31a6bb28
Alexandr Monastyretsky <alex.mon1989@gmail.com>
2016-05-18 22:14:35 +0300

    fix: настройки ключа google maps api.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,8 +7,15 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+use Illuminate\Support\Facades\Config;
+use Orchestra\Support\Facades\Memory;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        Config::set('googlemaps.key', Memory::get('GOOGLE_MAPS_API_KEY', env('GOOGLE_MAPS_API_KEY', 'AIzaSyC8Mxed4trkdkkJjucBbf376lMhYRxIVdE')));
+    }
 }
