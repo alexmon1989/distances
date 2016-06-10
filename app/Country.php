0d75b26a04b3245a2de13571bf23cc10a9168a56
alexmon1989 <alex.mon1989@gmail.com>
2016-06-10 12:26:11 +0300

    fix: редактирование настроек по умолчанию для калькулятора стоимости поездки.

--- app/Country.php
+++ app/Country.php
@@ -9,7 +9,16 @@ class Country extends Model
     use \Dimsav\Translatable\Translatable;
 
     public $translatedAttributes = ['name'];
-    protected $fillable = ['code', 'name', 'is_enabled'];
+    protected $fillable = [
+        'code',
+        'name',
+        'is_enabled',
+        'distance_unit',
+        'volume_unit',
+        'fuel_consumption',
+        'fuel_cost',
+        'currency',
+    ];
 
     /**
      * The relations to eager load on every query.
--- database/seeds/CountriesTableSeeder.php
+++ database/seeds/CountriesTableSeeder.php
@@ -23,6 +23,11 @@ class CountriesTableSeeder extends Seeder
             'en'  => ['name' => 'Russia'],
             'uk'  => ['name' => 'Росія'],
             'pl'  => ['name' => 'Rosja'],
+            'distance_unit' => 'kilometer',
+            'volume_unit' => 'liter',
+            'fuel_consumption' => 10,
+            'fuel_cost' => 35,
+            'currency' => 'ruble',
         ];
         $russia = Country::create($russiaData);
 
@@ -33,6 +38,11 @@ class CountriesTableSeeder extends Seeder
             'en'  => ['name' => 'United States of America'],
             'uk'  => ['name' => 'Сполучені Штати Америки'],
             'pl'  => ['name' => 'USA'],
+            'distance_unit' => 'mile',
+            'volume_unit' => 'us_gallon',
+            'fuel_consumption' => 5,
+            'fuel_cost' => 2,
+            'currency' => 'us_dollar',
         ];
         $usa = Country::create($usaData);
 
@@ -43,6 +53,11 @@ class CountriesTableSeeder extends Seeder
             'en'  => ['name' => 'Ukraine'],
             'uk'  => ['name' => 'Україна'],
             'pl'  => ['name' => 'Ukraina'],
+            'distance_unit' => 'kilometer',
+            'volume_unit' => 'liter',
+            'fuel_consumption' => 10,
+            'fuel_cost' => 22,
+            'currency' => 'uah',
         ];
         $ukraine = Country::create($ukraineData);
 
@@ -53,6 +68,11 @@ class CountriesTableSeeder extends Seeder
             'en'  => ['name' => 'Poland'],
             'uk'  => ['name' => 'Польща'],
             'pl'  => ['name' => 'Polska'],
+            'distance_unit' => 'kilometer',
+            'volume_unit' => 'liter',
+            'fuel_consumption' => 10,
+            'fuel_cost' => 1.5,
+            'currency' => 'euro',
         ];
         $poland = Country::create($polandData);
     }
